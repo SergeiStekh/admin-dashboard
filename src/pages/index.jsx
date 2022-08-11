d45be6e0f5c7396c59b01e3b7077a500c1f907b6
SergeyCodeJs <sergeysteh@gmail.com>
2022-08-11 13:25:34 +0200

    feat: add templates for Customers, Employees, Ecommerce, Editor, Kanban and Orders pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,10 @@
 export { default as Calendar } from './Calendar'
+export { default as Customers } from './Customers'
+export { default as Ecommerce } from './Ecommerce'
+export { default as Editor } from './Editor'
+export { default as Employees } from './Employees'
+export { default as Kanban } from './Kanban'
+export { default as Orders } from './Orders'
 
 export { default as Area } from './Diagrams/Area'
 export { default as Bar } from './Diagrams/Bar'
