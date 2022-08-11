0a595fd0c940aa1710e9951395660940c3cb23c3
SergeyCodeJs <sergeysteh@gmail.com>
2022-08-11 14:44:04 +0200

    feat: add sidebar styles and elements

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {FiSettings as SettingsIcon} from 'react-icons/fi'
 import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
+import {Calendar, Customers, Ecommerce, Editor, Employees, Kanban, Orders, Area, Bar, Financial, Line, Pie, Pyramid, Stacked } from './pages'
 import './App.css'
 
 const App = () => {
-    const isMenuActive = false;
+    const isMenuActive = true;
     return (
         <Router>
             <div className='flex relative dark:bg-main-dark-bg'>
@@ -27,41 +29,40 @@ const App = () => {
                     </TooltipComponent>
                 </div>
                 {isMenuActive
-                    ? <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>Sidebar</div>
-                    : <div className='w-0 dark:bg-secondary-dark-bg'>Sidebar</div>}
+                    ? <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'><Sidebar/></div>
+                    : <div className='w-0 dark:bg-secondary-dark-bg'><Sidebar/></div>}
                 <div
                     className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${isMenuActive
                     ? 'md:ml-72'
                     : 'flex-2'}`}>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
-                        Navbar
+                        <Navbar/>
                     </div>
                 </div>
                 <div>
-                  <Routes>
-                    {/* DashBoard */}
-                    <Route path='/' element="E-commerce"/>
-                    <Route path='/ecommerce' element="E-commerce"/> 
+                    <Routes>
+                        <Route path='/' element={<Ecommerce/>}/>
+                        <Route path='/ecommerce' element={<Ecommerce/>}/> 
 
-                    {/* Pages */}
-                    <Route path='/orders' element="orders"/>
-                    <Route path='/employees' element="Employees"/>
-                    <Route path='/customers' element="Customers"/>
+                        {/* Pages */}
+                        <Route path='/orders' element={<Orders/>}/>
+                        <Route path='/employees' element={<Employees/>}/>
+                        <Route path='/customers' element={<Customers/>}/> 
 
-                    {/* Apps */}
-                    <Route path='/kanban' element="Kanban"/>
-                    <Route path='/editor' element="Editor"/>
-                    <Route path='/calendar' element="Calendar"/>
-
-                    {/* Diagrams */}
-                    <Route path='/linear' element="Linear"/>
-                    <Route path='/area' element="Area"/>
-                    <Route path='/bar' element="Bar"/>
-                    <Route path='/pie' element="Pie"/>
-                    <Route path='/financial' element="Financial"/>
-                    <Route path='/pyramid' element="Pyramid"/>
-                    <Route path='/stacked' element="Stacked"/>
-                  </Routes>
+                        {/* Apps */}
+                        <Route path='/kanban' element={<Kanban/>}/>
+                        <Route path='/editor' element={<Editor/>}/>
+                        <Route path='/calendar' element={<Calendar/>}/> 
+                        
+                        {/* Diagrams */}
+                        <Route path='/linear' element={<Line/>}/>
+                        <Route path='/area' element={<Area/>}/>
+                        <Route path='/bar' element={<Bar/>}/>
+                        <Route path='/pie' element={<Pie/>}/>
+                        <Route path='/financial' element={<Financial/>}/>
+                        <Route path='/pyramid' element={<Pyramid/>}/>
+                        <Route path='/stacked' element={<Stacked/>}/>
+                    </Routes>
                 </div>
             </div>
         </Router>
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Employes = () => {
   return (
-    <div>Employes</div>
+    <div>Employees</div>
   )
 }
 
