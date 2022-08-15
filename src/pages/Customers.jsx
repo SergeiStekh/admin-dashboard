8393de817ffa62d4ad99d8dbd893247900bc1089
SergeyCodeJs <sergeysteh@gmail.com>
2022-08-15 11:13:22 +0200

    feat: add Customers page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+import { customersData } from '../data/customersData'
+import { customersGrid } from '../data/customersGrid'
+import { Header } from '../components'
+
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent 
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete']}
+        editSettings={{allowDeleting: true, allowEditing: true}}
+        width="auto"
+        >
+        <ColumnsDirective>
+          {customersGrid.map((item, idx) => (
+            <ColumnDirective key={idx} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]}/>
+      </GridComponent>
+    </div>
   )
 }
 
